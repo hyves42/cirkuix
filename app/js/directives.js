dfd3bd19aba5aa06ed6d907fb5757ec5df58051c
hyves42 <hyves42@users.noreply.github.com>
2014-04-15 23:32:51 +0200

    Add directive elec-numbers-only to allow only electronics numbers with units

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,56 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('myApp.directives', [])
+	.directive('appVersion', ['version', function(version) {
+		return function(scope, elm, attrs) {
+		elm.text(version);
+		};
+	}])
+
+	.directive('elecNumbersOnly', function(){
+		return {
+			restrict: 'AE',
+			require: 'ngModel',
+
+			link: function(scope, element, attrs, ngModel) {
+
+				element.on('blur', function() {
+					scope.$apply(function () {
+						ngModel.$setViewValue(unitStringfromFloat(scope[attrs.ngModel+"_float"]));
+						ngModel.$render();
+						ngModel.$setValidity('format', true);
+					});
+				});
+
+				ngModel.$parsers.push(function (inputValue) {
+
+					if (inputValue == undefined) return '' 
+
+					var filteredInput = inputValue.replace(/[^0-9,^TGMkmunp,^\\.,^µ]/g, ''); 
+					if (filteredInput!=inputValue) {
+						ngModel.$setViewValue(filteredInput);
+						ngModel.$render();
+					}
+
+
+					try{
+						var r = floatFromUnitString(filteredInput);
+						ngModel.$setValidity('format', true);
+						scope[attrs.ngModel+"_float"] = r
+						return filteredInput;
+					}
+					catch(err){
+						ngModel.$setValidity('format', false);
+						scope[attrs.ngModel+"_float"] = undefined;
+					//	return undefined;
+					}
+
+					return filteredInput;
+
+				});
+
+			}
+		};
+	});
+
