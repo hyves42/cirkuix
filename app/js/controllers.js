a13e0ee884c22223d9dc79e11cecc310b4050a0d
hyves42 <hyves42@users.noreply.github.com>
2014-04-17 00:34:34 +0200

    UI improvements tests with bootstrap

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,43 +9,14 @@ angular.module('myApp.controllers', [])
   .controller('FiltersRC', ['$scope', function($scope) {
 
 	$scope.r1 = "100k";
-	$scope.c1 = "20µ";
-
+	$scope.c1 = "20u";
+	$scope.tau=0;
 	
 	$scope.updateCircuitValues = function () {
 		//Dummy test
 		$scope.tau += 1;
 	};
 
-	$scope.updateR = function () {
-		
-		var str = $scope.r_str;
-		var r =0;
-
-		try{
-			r = floatFromUnitString(str);
-			$scope.r = r;
-			$scope.r_str = unitStringfromFloat(r);
-		}
-		catch(err){
-			$scope.r_str = "err"
-		}
-	};
-
-	$scope.updateC = function () {
-
-		var str = $scope.c_str;
-		var c=0;
-
-		try{
-			c = floatFromUnitString(str);
-			$scope.c = c;
-			$scope.c_str = unitStringfromFloat(c);
-		}
-		catch(err){
-			$scope.c_str = "err"
-		}
-	};
 
   }])
   .controller('MyCtrl2', [function() {
