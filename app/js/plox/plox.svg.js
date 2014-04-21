191d90133f5f699e13245dfb9cf775582313b693
hyves42 <hyves42@users.noreply.github.com>
2014-04-21 13:02:47 +0200

    Get rid of the svg transform to convert coordinates

--- app/js/plox/plox.svg.js
+++ app/js/plox/plox.svg.js
@@ -1,12 +1,42 @@
 angular.module('plox', [])
 	.filter('topath', function() {
-		return function(data) {
+		return function(data, graph) {
+			var h = graph.height;
+			var ymin = graph.yProperties.min;
+			var ymax = graph.yProperties.max;
+
+			var w = graph.width;
+			var xmin = graph.xProperties.min;
+			var xmax = graph.xProperties.max;	
+
 			var out ="M "
 			for (var i=0; i < data.length; i++){
 				var p = data[i];
-				out += p.x + ' ' + p.y + ' ';
+
+				var x = p.x * (w-20) / (xmax-xmin) + 10 +xmin*(w-20)/(xmin-xmax);
+				var y = p.y * (h-20) / (ymin-ymax) + 10 + ymax * (h-20) / (ymax-ymin);
+
+				out += x + ' ' + y + ' ';
 			}
 			return out;
+		};
+	})
+	.filter('convertX', function() {
+		return function(x, graph) {
+			var w = graph.width;
+			var xmin = graph.xProperties.min;
+			var xmax = graph.xProperties.max;			
+
+			return x * (w-20) / (xmax-xmin) + 10 +xmin*(w-20)/(xmin-xmax);
+		};
+	})
+	.filter('convertY', function() {
+		return function(y, graph) {
+			var h = graph.height;
+			var ymin = graph.yProperties.min;
+			var ymax = graph.yProperties.max;
+
+			return y * (h-20) / (ymin-ymax) + 10 + ymax * (h-20) / (ymax-ymin);
 		};
 	})
 	.directive('ploxSvg', function() {
@@ -18,48 +48,96 @@ angular.module('plox', [])
 			replace: 'true',
 			template: 
 '<svg ng-attr-width="{{graph.width}}" ng-attr-height="{{graph.height}}">'
-+	'<g ng-attr-transform="matrix('
-+		'{{ graph.width / (graph.xProperties.max-graph.xProperties.min)}}, 0, 0,'
-+		'{{ graph.height / (graph.yProperties.min-graph.yProperties.max)}},'
-+		'{{ graph.xProperties.min * graph.width / (graph.xProperties.min-graph.xProperties.max)}},'
-+		'{{ graph.yProperties.max * graph.height / (graph.yProperties.max-graph.yProperties.min)}})">'
 
-+		'<g ng-switch="graph.xProperties.drawAxis">'
-+			'<g ng-switch-when="true">'
-+				'<path ng-attr-d="M {{graph.xProperties.min}} 0 H {{ graph.xProperties.max-graph.xProperties.min }}"'
-+				' style="fill:none;stroke:#000000;stroke-width:{{(graph.xProperties.max-graph.xProperties.min)/graph.width}}"/>'
-
-+				'<path ng-attr-d="M {{graph.xProperties.max - 3}} 1 l 3 -1 l -3 -1"'
-+				' style="fill:none;stroke:#000000;stroke-width:{{(graph.xProperties.max-graph.xProperties.min)/graph.width}}"/>'
-
-+			'</g>'			
-+			'<g ng-switch-default>'
-+			'</g>'
++	'<g ng-switch="graph.xProperties.grid">'
++		'<g ng-switch-when="dashed">'
++			'<path ng-repeat="g in graph.xGrid" '
++				'ng-attr-d="M {{g.x | convertX:graph}} {{graph.yProperties.min | convertY:graph}} '
++					'L {{g.x | convertX:graph}} {{graph.yProperties.max | convertY:graph}}" '
++				'style="fill:none;stroke:#{{graph.xProperties.gridColor}};'
++				'stroke-width:1;stroke-dasharray:3,3"/>'
 +		'</g>'
-
-+		'<g ng-switch="graph.yProperties.drawAxis">'
-+			'<g ng-switch-when="true">'
-+				'<path ng-attr-d="M 0 {{graph.yProperties.min}} V {{ graph.yProperties.max-graph.yProperties.min }}"'
-+				' style="fill:none;stroke:#000000;stroke-width:{{(graph.xProperties.max-graph.xProperties.min)/graph.width}}"/>'
-
-+				'<path ng-attr-d="M 1 {{graph.yProperties.max - 3}} l -1 3 l -1 -3"'
-+				' style="fill:none;stroke:#000000;stroke-width:{{(graph.xProperties.max-graph.xProperties.min)/graph.width}}"/>'
-
-+			'</g>'			
-+			'<g ng-switch-default>'
-+			'</g>'
++		'<g ng-switch-when="solid">'
++			'<path ng-repeat="g in graph.xGrid" '
++				'ng-attr-d="M {{g.x | convertX:graph}} {{graph.yProperties.min | convertY:graph}} '
++					'L {{g.x | convertX:graph}} {{graph.yProperties.max | convertY:graph}}" '
++				'style="fill:none;stroke:#{{graph.xProperties.gridColor}};'
++				'stroke-width:1;"/>'
 +		'</g>'
-
-+		'<path ng-repeat="g in graph.xGrid" ng-attr-d="M {{g.x}} {{graph.yProperties.min}} V {{ graph.yProperties.max-graph.yProperties.min }}" '
-+			'style="fill:none;stroke:#222222;stroke-width:{{(graph.xProperties.max-graph.xProperties.min)/graph.width}};stroke-dasharray:1,1"/>'
-
-+		'<path ng-repeat="g in graph.yGrid" ng-attr-d="M {{graph.xProperties.min}} {{g.y}} H {{ graph.xProperties.max-graph.xProperties.min }}" '
-+			'style="fill:none;stroke:#222222;stroke-width:{{(graph.xProperties.max-graph.xProperties.min)/graph.width}};stroke-dasharray:1,1"/>'
-
-
-+		'<path ng-repeat="d in graph.data" ng-attr-d="{{ d.points | topath }}" '
-+			'style="fill:none;stroke:#{{ d.color }};stroke-width:{{d.width*(graph.xProperties.max-graph.xProperties.min)/graph.width}};stroke-linecap:round"/>'
++		'<g ng-switch-when="caret">'
++			'<path ng-repeat="g in graph.xGrid" '
++				'ng-attr-d="M {{g.x | convertX:graph}} {{(0 | convertY:graph) - 2}} '
++					'l 0 4" '
++				'style="fill:none;stroke:#000000;'
++				'stroke-width:1;"/>'
++		'</g>'
++		'<g ng-switch-default>'
++		'</g>'
 +	'</g>'
+
++	'<g ng-switch="graph.yProperties.grid">'
++		'<g ng-switch-when="dashed">'
+
++			'<path ng-repeat="g in graph.yGrid" '
++				'ng-attr-d="M {{graph.xProperties.min | convertX:graph}} {{g.y | convertY:graph}} '
++					' L {{graph.xProperties.max | convertX:graph}} {{g.y | convertY:graph}}" '
++				'style="fill:none;stroke:#{{graph.yProperties.gridColor}};'
++				'stroke-width:1;stroke-dasharray:3,3"/>'
++		'</g>'
++		'<g ng-switch-when="solid">'
++			'<path ng-repeat="g in graph.yGrid" '
++				'ng-attr-d="M {{graph.xProperties.min | convertX:graph}} {{g.y | convertY:graph}} '
++					' L {{graph.xProperties.max | convertX:graph}} {{g.y | convertY:graph}}" '
++				'style="fill:none;stroke:#{{graph.yProperties.gridColor}};'
++				'stroke-width:1;"/>'
++		'</g>'
++		'<g ng-switch-when="caret">'
++			'<path ng-repeat="g in graph.yGrid" '
++				'ng-attr-d="M {{(0 | convertX:graph)-2 }} {{g.y | convertY:graph}} '
++					' l 4 0" '
++				'style="fill:none;stroke:#000000;'
++				'stroke-width:1;"/>'
++		'</g>'
++		'<g ng-switch-default>'
++		'</g>'
++	'</g>'
+
++	'<g ng-switch="graph.xProperties.drawAxis">'
++		'<g ng-switch-when="true">'
++			'<path ng-attr-d="M {{graph.xProperties.min | convertX:graph }} {{ 0| convertY:graph }} '
++				'L {{(graph.xProperties.max | convertX:graph) + 6}} {{ 0| convertY:graph }}"'
++			' style="fill:none;stroke:#000000;stroke-width:1"/>'
+
++			'<path ng-attr-d="M {{(graph.xProperties.max | convertX:graph) + 6 }} {{ 0 | convertY:graph }} l -5 -2"'
++			' style="fill:none;stroke:#000000;stroke-width:1"/>'
++			'<path ng-attr-d="M {{(graph.xProperties.max | convertX:graph) + 6 }} {{ 0 | convertY:graph }} l -5 2"'
++			' style="fill:none;stroke:#000000;stroke-width:1"/>'
+
++		'</g>'			
++		'<g ng-switch-default>'
++		'</g>'
++	'</g>'
+
++	'<g ng-switch="graph.yProperties.drawAxis">'
++		'<g ng-switch-when="true">'
++			'<path ng-attr-d="M {{ 0 | convertX:graph }} {{graph.yProperties.min | convertY:graph}} '
++				' L {{0 | convertX:graph }} {{ (graph.yProperties.max| convertY:graph) -6 }}"'
++			' style="fill:none;stroke:#000000;stroke-width:1"/>'
+
++			'<path ng-attr-d="M {{ 0 | convertX:graph }} {{(graph.yProperties.max | convertY:graph) -6}} l -2 5"'
++			' style="fill:none;stroke:#000000;stroke-width:1"/>'
+
++			'<path ng-attr-d="M {{ 0 | convertX:graph }} {{(graph.yProperties.max | convertY:graph) -6}} l 2 5"'
++			' style="fill:none;stroke:#000000;stroke-width:1"/>'
+
++		'</g>'			
++		'<g ng-switch-default>'
++		'</g>'
++	'</g>'
+
++	'<path ng-repeat="d in graph.data" ng-attr-d="{{ d.points | topath:graph }}" '
++		'style="fill:none;stroke:#{{ d.color }};stroke-width:{{d.width}};stroke-linecap:round"/>'
+
 +'</svg>'
 		};
 	})
